7b25e4ddaa51a6801a407e684f448c68cb188c68
Alina Aftab <alina.aftab2001@gmail.com>
2024-03-12 23:06:01 +0500

    Main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+class Mlops:
+	def __init__(self,totalStudents):
+		self.totalStudents=totalStudents
+	
+	def getTotalStudents(self):
+		return self.totalStudents
+
+	def addStudent(self):
+		self.totalStudents=self.totalStudents + 1
+
+	def removeStudent(self):
+		self.totalStudents=self.totalStudents - 1
+
+	def getClassName(self):
+		return "Machine Leraning Opertaions CS-B"
+
+
